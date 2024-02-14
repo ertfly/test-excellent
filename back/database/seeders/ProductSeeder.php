d9bc68b0a19cf39ade0a826d2cec08cebea1305f
Eric Teixeira <ertfly@gmail.com>
2024-02-14 18:18:22 -0300

    add seeder product

---
+++ back/database/seeders/ProductSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Products;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Products::create([
+            'name' => 'Produto 2',
+            'price' => 150.0
+        ]);
+
+        Products::create([
+            'name' => 'Produto 3',
+            'price' => 150.0
+        ]);
+
+        Products::create([
+            'name' => 'Produto 4',
+            'price' => 150.0
+        ]);
+    }
+}
